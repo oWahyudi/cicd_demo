b797223f535ae53fb0d9e71a64b7d5ac55e2dfb5
Onggo Wahyudi <carnegzs@gmail.com>
2024-01-23 02:01:02 +0800

    fix vulnerability issue

--- src/main/java/sg/edu/nus/iss/cicd_demo/controller/DataController.java
+++ src/main/java/sg/edu/nus/iss/cicd_demo/controller/DataController.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.github.javafaker.Faker;
+import net.datafaker.Faker;
 
 @RestController
 public class DataController {

1b873c730603f3bd0996cf260eabf03e8e5f17e7
Onggo Wahyudi <carnegzs@gmail.com>
2024-01-24 11:06:30 +0800

    fix codesmell #2  issue

--- src/test/java/sg/edu/nus/iss/cicd_demo/CicdDemoApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/cicd_demo/CicdDemoApplicationTests.java
@@ -1,14 +1,19 @@
 package sg.edu.nus.iss.cicd_demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CicdDemoApplicationTests {
 
+    @Autowired
+    private CicdDemoApplication applicationContext;
+
     @Test
     void contextLoads() {
-
+        assertNotNull(applicationContext, "Application context should not be null");
     }
 
 }
